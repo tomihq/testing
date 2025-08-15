d4c366987dadbbf278c149f0869d7f5cb633c7c6
= <hernandeztomas584@gmail.com>
2025-08-14 22:13:46 -0300

    add: NaN result

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,16 +1,28 @@
 import { it, expect } from 'vitest'
 import { add } from './math'
 
-//This test follows the AAA pattern.
+//BAD TEST. 
+it('should summarize all number values in an array', () => {
+    const result = add([1, 2, 3]);
+    expect(result).toBe(6)
+})
+
+//This test follows the AAA pattern. Good test.
 it('should summarize all number values in an array', () => {
     //Arrange
     const arr = [1, 2, 3];
-    const expectedResult = arr.reduce((prev, current) =>{prev+current}, 0)
     
     //Act
     const result = add(arr)
 
     //Assert
+    const expectedResult = arr.reduce((prev, current) => prev+current, 0)
     expect(result).toBe(expectedResult)
 })
 
+
+it('should yield NaN if a least one invalid number is provided', () =>{
+    const arr = [1, undefined];
+    const result = add(arr);
+    expect(result).toBe(NaN)
+})
