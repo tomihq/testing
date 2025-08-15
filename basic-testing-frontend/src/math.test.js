a0db38caca4b800d92e0b76a2e9a0a59d56bf5bb
= <hernandeztomas584@gmail.com>
2025-08-14 21:44:39 -0300

    add: test with AAA pattern

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,8 +1,16 @@
 import { it, expect } from 'vitest'
 import { add } from './math'
 
+//This test follows the AAA pattern.
 it('should summarize all number values in an array', () => {
-    const result = add([1, 2, 3])
-    expect(result).toBe(6)
+    //Arrange
+    const arr = [1, 2, 3];
+    const expectedResult = arr.reduce((prev, current) =>{prev+current}, 0)
+    
+    //Act
+    const result = add(arr)
+
+    //Assert
+    expect(result).toBe(expectedResult)
 })
 
