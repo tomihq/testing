bd75b0d1de9917d50c3b8baf29bc3adc644b2c0f
Tom <hernandeztomas584@gmail.com>
2025-08-15 15:35:00 -0300

    add: multiple assert

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -2,22 +2,25 @@ import { it, expect } from "vitest";
 import { transformToNumber } from './numbers'
 
 it('should convert a string number to number of type number', () =>{
-    const sr = "1";
-    const result = transformToNumber(sr);
+    const input = "1";
+    const result = transformToNumber(input);
     expect(result).toBeTypeOf('number');
 })
 
 it('should convert a string to number', () =>{
-    const sr = "1";
+    const input = "1";
     const expectedResult = 1; 
-    const result = transformToNumber(sr);
+    const result = transformToNumber(input);
     expect(result).toBe(expectedResult);
 })
 
 it('should return NaN if argument provided is not casteable', () =>{
-    const sr = "p";
-    const result = transformToNumber(sr);
+    const input = "p";
+    const input2 = {};
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
     expect(result).toBeNaN();
+    expect(result2).toBeNaN()
 })
 
 it('should return NaN if no argument is provided', () =>{
