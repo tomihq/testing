65edea990bcdf44aa05fd309bada69287b9d38f2
Tom <hernandeztomas584@gmail.com>
2025-08-15 21:05:02 -0300

    add: custom mocks

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,9 +1,23 @@
-import { it, expect } from 'vitest'
+import { it, expect, vi } from 'vitest'
+import { promises as fs } from 'fs'
 import writeData from './io'
+vi.mock('fs')
+vi.mock('path', () =>{
+    return {
+        default: {
+            join: (...args) =>{
+                return args[args.length - 1]
+            }
+        }
+    }
+})
 
 it('should execute the writeFile method', () =>{
     const testData = 'Test';
     const testFilename = "test.txt"
-    return expect(writeData(testData, testFilename)).resolves.toBeUndefined()
+    writeData(testData, testFilename)
+    //expect(fs.writeFile).toBeCalled()
+    /* return expect(writeData(testData, testFilename)).resolves.toBeUndefined() */
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData) 
 })
 
