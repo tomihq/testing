d86f559a855236f9b7938c93206a6bd0395e426e
Tom <hernandeztomas584@gmail.com>
2025-08-16 18:50:18 -0300

    add: mocking fetch

--- util/http.test.js
+++ util/http.test.js
@@ -1,11 +1,61 @@
-import { it, vi} from 'vitest';
+import { it, vi, expect} from 'vitest';
 
 import { sendDataRequest } from './http';
+import { HttpError } from './errors';
 
-vi.stubGlobal('fetch', () =>{
-    
-})
+const testResponseData = {testKey: 'testData'}
+const testFetch = vi.fn((url, options) => {
+    return new Promise((resolve, reject) => {
+        if(typeof options.body !== 'string'){
+            return reject('Not a string')
+        }
+        const testResponse = {
+            ok: true,
+            json() {
+                return new Promise((resolve, reject) =>{
+                    resolve(testResponseData)
+                })
+            }
+        };
+        resolve(testResponse);
+    })
+});
+vi.stubGlobal('fetch', testFetch)
 
 it('should return any available response data', () =>{
-    sendDataRequest()
+    const testData = {key: 'test'}
+    
+    return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData)
 })
+
+it('should convert the provided data to JSON before sending the request', async() =>{
+    const testData = {key: 'test'}
+    let errorMessage
+     try {
+        await sendDataRequest(testData)
+     } catch (error) {
+        errorMessage = error
+     }
+
+     expect(errorMessage).not.toBe('Not a string')
+
+})
+
+it('should throw an HttpError in case of non-ok responses', () =>{
+    testFetch.mockImplementationOnce((url, options) => {
+    return new Promise((resolve, reject) => {
+        const testResponse = {
+            ok: false,
+            json() {
+                return new Promise((resolve, reject) =>{
+                    resolve(testResponseData)
+                })
+            }
+        };
+        resolve(testResponse);
+    })
+})
+    const testData = {key: 'test'}
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError)
+})
+
