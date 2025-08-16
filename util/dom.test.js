16464a2f18ae2990c2e12d69bccc401b559b9277
Tom <hernandeztomas584@gmail.com>
2025-08-16 19:25:09 -0300

    add: unit testing end

---
+++ util/dom.test.js
@@ -0,0 +1,42 @@
+import { it, vi, expect, beforeEach, test} from 'vitest'
+import fs from 'fs';
+import path from 'path';
+
+import { showError } from './dom.js';
+import { Window } from 'happy-dom'
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+vi.stubGlobal('document', document)
+
+beforeEach(() =>{
+    document.body.innerHTML = '';
+    document.write(htmlDocumentContent);
+})
+
+it('should add an error paragraph to the id="errors" element', () =>{
+    showError("Test")
+    const errorsEl = document.getElementById('errors')
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).not.toBeNull()
+})
+
+it('should not contain an error paragraph initially', () =>{
+    const errorsEl = document.getElementById('errors')
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).toBeNull()
+})
+
+it('should output the provided message in the error paragraph', () =>{
+    const testErrorMessage = 'Test';
+    showError(testErrorMessage);
+    const errorsEl = document.getElementById('errors')
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph.textContent).toBe(testErrorMessage)
+})
